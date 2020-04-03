0a7d6c4752184b9991efe9ac68eddba52cfa4b89
ochappy <63106441+ochappy@users.noreply.github.com>
2020-04-03 14:22:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// javascript
+
+// a programming language... that allows us to use logic in websites
+
+// variable... this is simply, a storage space for us...
+
+let name = "Oliver";
+let age = "14";
+let school = "Blue Coat School";
+
+console.log("Hi, my name is " + name);
+name = "Imran";
+console.log("Hi, my name is " + name);
+
+console.log("I am " + age)
+console.log("I attend " + school)
+
+let numberOne;
+let numberTwo;
+let total;
+
+const addition = (numberOne, numberTwo) => {
+       total = numberOne + numberTwo;
+       console.log(total)
+
+};
+
+addition(20, 30);
+addition(30, 70);
+addition(40, 15);
+addition(400,150);
